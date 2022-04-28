37c16f118465601d717c84949ae2e1377ef1c494
Chanhook <cksgur97@gmail.com>
2022-04-28 03:07:10 +0000

    [feat] update_dataset 함수 추가

--- utils/split_stratified_dataset.py
+++ utils/split_stratified_dataset.py
@@ -3,6 +3,7 @@ import json
 import numpy as np
 import pandas as pd
 import argparse
+import random
 
 from tqdm import tqdm
 
@@ -15,6 +16,7 @@ annotations_path = os.path.join(data_path, "train_all.json")
 
 
 def main(args):
+    random.seed(args.random_seed)
     with open(annotations_path, "r") as f:
         train_json = json.loads(f.read())
         images = train_json["images"]
@@ -49,14 +51,14 @@ def main(args):
 
         train_dict["images"] = np.array(images)[train_index].tolist()
         val_dict["images"] = np.array(images)[val_index].tolist()
-
+        
         train_dict["annotations"] = annotations_df[
             annotations_df["image_id"].isin(train_index)
         ].to_dict("records")
         val_dict["annotations"] = annotations_df[
             annotations_df["image_id"].isin(val_index)
         ].to_dict("records")
-
+        
         train_dir = os.path.join(path, f"train_fold{idx}.json")
         val_dir = os.path.join(path, f"val_fold{idx}.json")
 
@@ -68,6 +70,60 @@ def main(args):
 
     print("Done Make files")
 
+def update_dataset(index, mode, input_json, output_dir):
+
+    with open(input_json) as json_reader:
+        dataset = json.load(json_reader)
+
+    images = dataset["images"]
+    annotations = dataset["annotations"]
+    categories = dataset["categories"]
+
+    image_ids = [x.get("id") for x in images]
+    image_ids.sort()
+
+    image_ids_train = set(image_ids)
+
+    train_images = [x for x in images if x.get("id") in image_ids_train]
+
+    train_id2id = dict()
+
+    for i in range(len(train_images)):
+        train_id2id[train_images[i]["id"]] = i
+        train_images[i]["id"] = i
+
+    train_annotations = [x for x in annotations if x.get("image_id") in image_ids_train]
+
+    for i in range(len(train_annotations)):
+        train_annotations[i]["image_id"] = train_id2id[train_annotations[i]["image_id"]]
+
+    train_data = {
+        "images": train_images,
+        "annotations": train_annotations,
+        "categories": categories,
+    }
+
+    output_train_json = os.path.join(output_dir, f"{mode}_fold{index}.json")
+
+    print(f"write {output_train_json}")
+    with open(output_train_json, "w") as train_writer:
+        json.dump(train_data, train_writer)
+
+def loop_n_split(n):
+    stratified_path = os.path.join(path, "..","data", "stratified_kfold")
+    for i in range(n):
+        update_dataset(
+            index=i,
+            mode="train",
+            input_json=os.path.join(stratified_path, f"train_fold{i}.json"),
+            output_dir=stratified_path,
+        )
+        update_dataset(
+            index=i,
+            mode="val",
+            input_json=os.path.join(stratified_path, f"val_fold{i}.json"),
+            output_dir=stratified_path,
+        )
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -77,6 +133,8 @@ if __name__ == "__main__":
         type=str,
         default=os.path.join(path, "..", "data", "stratified_kfold"),
     )
-    parser.add_argument("--n_split", "-n", type=int, default=10)
+    parser.add_argument("--n_split", "-n", type=int, default=5)
+    parser.add_argument("--random_seed", type=int, default=42)
     args = parser.parse_args()
     main(args)
+    loop_n_split(args.n_split)
